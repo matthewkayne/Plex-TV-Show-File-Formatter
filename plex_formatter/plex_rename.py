1cccf49db01a85f8c9b022c2a6d98a6dd7bcd318
Matthew Kayne <matthewkayne2004@gmail.com>
2022-03-20 23:48:11 +0000

    pylint

--- plex_formatter/plex_rename.py
+++ plex_formatter/plex_rename.py
@@ -1,6 +1,9 @@
+"""Plex Rename"""
 import os
 
+
 def rename():
+    """Rename"""
     path = input("Path to folder (Enter if current directory): ")
     showname = input("Show Name: ")
     season = int(input("Season: "))
@@ -8,25 +11,26 @@ def rename():
 
     if path == "":
         directory = [f for f in os.listdir() if not f.startswith('.')]
-        directory.remove("file-rename.py")     
-    else:  
+        directory.remove("file-rename.py")
+    else:
         directory = [f for f in os.listdir(path) if not f.startswith('.')]
-      
+
     files = len(directory)
 
     for i in range(files):
         if season < 10:
-            seasonString="0"+str(season)
+            seasonString = "0"+str(season)
         else:
             seasonString = str(season)
         if i < 10:
-            episodeString="0"+str(i+1)
+            episodeString = "0"+str(i+1)
         else:
-            episodeString=str(i+1)
-            
+            episodeString = str(i+1)
+
         filename = f"{path}{showname} - s{seasonString}e{episodeString}.{filetype}"
-    
+
         os.rename(f"{path}{directory[i]}", filename)
+
 
 if __name__ == "__main__":
     rename()
